a8942cb51916b5c1faa3b792a64cbef1641cd561
picpoint <picpoint@yandex.ru>
2020-08-18 22:52:52 +0300

    work with GET parametrs

---
+++ src/index.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Home page</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <h3>Home page</h3>
+</body> 
+</html>
