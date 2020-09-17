f2afc2e0b37c8490fe747919b756690547c2f4de
picpoint <picpoint@yandex.ru>
2020-09-17 21:25:49 +0400

    work with authorization

---
+++ dist/authorization.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Authorization</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+  <section class="auth">
+    <div class="auth__wrp">
+      <div class="auth__headline">
+        <h3>Авторизация</h3>
+      </div>
+      <div class="auth__blockform">
+        <form method="POST" class="auth__authoriz">
+          <input type="text" name="login">
+          <input type="text" name="password">
+          <button type="submit">Войти</button>
+        </form>        
+      </div>
+    </div>
+  </section>
+
+</body>
+</html>
--- src/userPage.php
+++ src/userPage.php
@@ -8,18 +8,36 @@ session_start();
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>User page</title>
+  <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-<?php
-  
-?>
 
-<h3>User page</h3>
-
-<?php
-  echo($_SESSION['user']);
-?>
-
+<section class="lk">
+  <div class="lk__wrp">
+    <div class="lk__headline">
+      <div class="lk__username">
+        <span>
+          <?php
+            echo($_SESSION['user']);
+          ?>
+        </span>
+      </div>
+      <div class="lk__menu">
+        <a href="#">Some link 1</a>
+        <a href="#">Some link 1</a>
+        <a href="#">Some link 1</a>
+      </div>
+      <div class="lk__outblock">
+        <a href="index.php">Выход</a>
+      </div>
+    </div>
+    <div class="lk__mainblock">
+      <div class="lk__blockform">
+        
+      </div>
+    </div>
+  </div>
+</section>
 
 
 </body>
