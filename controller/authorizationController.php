60dee8304afe29ee4669f583c9c216ef7623a490
picpoint <picpoint@yandex.ru>
2020-09-22 23:27:03 +0400

    work with auth

--- controller/authorizationController.php
+++ controller/authorizationController.php
@@ -6,6 +6,10 @@ class AuthorizC {
 
 
   public function makeAuthoriz() {
+    $arrLogs = [];
+    $arrPass = [];
+    $arrCombine = [];
+
     if(isset($_POST['enteringbtn']) && !empty($_POST['enterlogin']) && !empty($_POST['enterpassword'])) {
       $enterlogin = trim($_POST['enterlogin']);
       $enterpassword = trim($_POST['enterpassword']);
@@ -13,17 +17,48 @@ class AuthorizC {
       echo($enterlogin);
       echo("<br>");
       echo($enterpassword);
+      echo("<br>");
+      echo("<br>");
 
       $usrsFromDB = new GetAllUsersM();
       $res = $usrsFromDB -> getAllUsrs();
 
+      // echo("<br>");
+      // echo("<br>");
+      // print_r($res);
+      // echo("<br>");
+      
       foreach($res as $rs) {
-        if(in_array($enterlogin, $rs)) {
-          echo("YES!!!!!!!!!!!!!!!");
-        } else {
-          echo('no ....');
+        foreach($rs as $key => $value) {
+          // echo("$key - $value");
+          echo("<br>");
+
+          if($key == 'login') {
+            $arrLogs[] = $value;
+          } else if($key == 'password') {
+            $arrPass[] = $value;
+          }
         }
       }
+
+      $arrCombine = array_combine($arrLogs, $arrPass);
+
+      // print_r($arrCombine);
+
+      foreach($arrCombine as $key => $value) {
+        echo("$key - $value");
+        echo("<br>");
+
+        if($enterlogin == $key && password_verify($enterpassword, $value)) {
+          echo("YESSSSS!!!!!!!!!!!!!!!!!");
+          echo("<br>");
+        } else {
+          echo("no .....");
+          echo("<br>");
+        }
+      }
+
+
 
     } else {
       echo("Поля не заполнены");
